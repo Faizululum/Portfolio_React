69dc9424217c2e18b4419abf1e7d1483acb3fca5
Faizululum <faizululum25@gmail.com>
2024-12-04 22:01:23 +0700

    Hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,102 @@
+import "./hero.scss";
+import { animate, motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    opacity: 1,
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 25,
+    },
+  },
+};
+const imgVariants = {
+  initial: {
+    y: -200,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>FAIZUL ULUM</motion.h2>
+          <motion.h1 variants={textVariants}>Web Developer</motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the Latest Work
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt="scroll"
+          />
+        </motion.div>
+        <motion.div
+          className="slidingTextContainer"
+          variants={sliderVariants}
+          initial="initial"
+          animate="animate"
+        >
+          Fullstack Web Developer
+        </motion.div>
+      </div>
+      <div className="imageContainer">
+        <motion.img
+          src="/hero.png"
+          alt="hero"
+          variants={imgVariants}
+          animate="animate"
+          initial="initial"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
