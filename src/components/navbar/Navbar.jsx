1452ee1547fcc5ea7b6dc7ccec8a8fffbe42a73a
Faizululum <faizululum25@gmail.com>
2024-12-04 10:23:55 +0700

    Sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
         {/* Sidebar */}
+        <Sidebar />
         <div className="wrapper">
-            <span>Faizululum Dev</span>
+            <motion.span 
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }} 
+              transition={{ duration: 0.5 }}
+            >
+              Faizululum Dev
+            </motion.span>  
             <div className="social">
                 <a href=""><img src="/linkedin.png" alt="" /></a>
                 <a href=""><img src="/github.png" alt="" /></a>
