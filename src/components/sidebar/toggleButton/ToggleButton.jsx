449951d37ca62f848d9774ecc09a4d0ee0a7fec7
Faizululum <faizululum25@gmail.com>
2024-12-04 20:45:58 +0700

    Sidebar menu animation

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,31 @@
-const ToggleButton = ({setOpen}) => {
-  return <button onClick={() => setOpen((prev) => !prev)}>Button</button>
-  
-}
+import { motion } from "framer-motion";
 
-export default ToggleButton
+const ToggleButton = ({ setOpen }) => {
+  return (
+    <button onClick={() => setOpen((prev) => !prev)}>
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
+        />
+        <motion.path
+          strokeWidth="3"
+          stroke="black"
+          strokeLinecap="round"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 },
+          }}
+        />
+        
+      </svg>
+    </button>
+  );
+};
+
+export default ToggleButton;
