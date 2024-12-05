cae6a64a9f685a43a2d8812ee900ba7e67ad4556
Faizululum <faizululum25@gmail.com>
2024-12-05 17:01:57 +0700

    useScroll & useTransform Hook using Framer Motion

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,5 @@
 import "./hero.scss";
-import { animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const textVariants = {
   initial: {
---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,44 @@
+import { useRef } from "react";
+import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <div
+      className="parallax"
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, var(--color-primary), var(--color-secondary)"
+            : "linear-gradient(180deg, var(--color-primary), #505064",
+      }}
+    >
+      <motion.h1 style={{ y: yText }}>
+        {type === "services" ? "What We Do" : "Our Work"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div
+        className="planets"
+        style={{
+          y: yBg,
+          backgroundImage: `url(${
+            type === "services" ? "/public/planets.png" : "/public/sun.png"
+          })`,
+        }}
+      ></motion.div>
+      <motion.div style={{ x: yBg }} className="stars"></motion.div>
+    </div>
+  );
+};
+
+export default Parallax;
